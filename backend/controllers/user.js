fabef28a4abee06536783f391399dfcf0c949955
Heneros <punk60826@gmail.com>
2024-02-09 14:07:46 +0200

    generateToken

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/Users");
 const asyncHandler = require("../middleware/asyncHandler");
+const generateToken = require("../utils/generateToken");
 
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
@@ -72,10 +73,13 @@ const deleteUser = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
+
+
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
     res.json({
       _id: user._id,
       name: user.name,
---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.cookie("blog_info", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "Lax",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+};
+
+module.exports = generateToken;
