5f94c5260b7bc0bac2b448db6723ddb7a7cf3ff3
Rustam <punk60826@gmail.com>
2024-05-27 10:15:41 +0300

    token model

---
+++ backend/models/verifyResetTokenModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+
+const {Schema } = mongoose;
+
+const verifyResetTokenSchema = new Schema({
+    _userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true, 
+        ref: 'User'
+    },
+     token: {type: String, required: true},
+     createdAt:{
+        type: Date,
+        required: true,
+        default: Date.now(),
+        expires:900
+     }
+})
